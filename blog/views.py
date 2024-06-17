b3c943c09cedec498c962318157cdd4b120760e3
Sean Freaney <seanfreaney123@gmail.com>
2024-06-17 19:44:32 +0000

    edit code in views.py

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,8 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
 
-    def post_detail(request, slug):
+
+def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
 
@@ -26,8 +27,4 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
 
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
